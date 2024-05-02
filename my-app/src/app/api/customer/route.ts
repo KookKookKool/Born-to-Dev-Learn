a22af7ede3b1ba58556d15c552de62da3119f171
KookKookKool <Sorakom.Aunglan@gmail.com>
2024-05-02 21:10:32 +0700

    d2

--- my-app/src/app/api/customer/route.ts
+++ my-app/src/app/api/customer/route.ts
@@ -1,16 +1,37 @@
-import { MongoClient } from "mongodb";
-import 'dotenv/config'
+
+import { Pool } from "pg";
+import { NextResponse } from "next/server";
+
+const pool = new Pool({
+    user: "root",
+    host: "postgredb",
+    database: "postgres",
+    password: "root",
+    port: 5433, // Use the port you mapped in Docker Compose
+  });
 
 export async function GET() {
-    const client = await MongoClient.connect(process.env.MONGODB_URI);
-    const db = client.db("sample_mflix"); //ใช้ชื่อในการเข้าถึง
-    const userCollection = db.collection("users"); //ไฟล์รองที่ต้องการเข้าถึง
-
-    const users = await userCollection.find({}).toArray();
-
-    return Response.json({ 
-        users
-    });
+    const query = "SELECT * FROM customers";
+    const { rows } = await pool.query(query);
+    return NextResponse.json(rows);
 }
+
+
+//MongoDB
+
+// import { MongoClient } from "mongodb";
+// import 'dotenv/config'
+
+// export async function GET() {
+//    const client = await MongoClient.connect(process.env.MONGODB_URI);
+//    const db = client.db("sample_mflix"); //ใช้ชื่อในการเข้าถึง
+//    const userCollection = db.collection("users"); //ไฟล์รองที่ต้องการเข้าถึง
+
+//    const users = await userCollection.find({}).toArray();
+
+//     return Response.json({ 
+//        users
+//    });
+// }
 
 //ต้องเพิ่ม ip ใน Network Access in MongoDB
