1e2d24430c4fc9c226c4bf2f11ae4ae212dfe415
KookKookKool <Sorakom.Aunglan@gmail.com>
2024-05-02 19:05:55 +0700

    Update route.ts

--- my-app/src/app/api/postgres/route.ts
+++ my-app/src/app/api/postgres/route.ts
@@ -1,19 +1,17 @@
-import { Pool } from 'pg';
-import { NextResponse } from 'next/server';
-import 'dotenv/config'
+import { Pool } from "pg";
+import { NextResponse } from "next/server";
 
+// GET ALL : Read
 export async function GET() {
-    const pool = new Pool({
-        user: "root",
-        host: "localhost",
-        database: "postgres",
-        password: "mypassword",
-        port: 8080,
-    });
+  const pool = new Pool({
+    user: "root",
+    host: "postgredb",
+    database: "postgres",
+    password: "root",
+    port: 5433, // Use the port you mapped in Docker Compose
+  });
 
-    const query = "SELECT * FROM customers";
-    const { rows } = await pool.query(query);
-
-    return NextResponse.json(rows);
+  const query = "SELECT * FROM customers";
+  const { rows } = await pool.query(query);
+  return NextResponse.json(rows);
 }
-
